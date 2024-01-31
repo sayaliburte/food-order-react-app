2fa71c77c1cf3645bfe1bb09b50a5b59573fde4d
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-01-31 22:31:31 +0530

    1)Added Meals files like Meals,MealsSummary,AvailableMeals.2)Added css to App file.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,19 @@
 import { Fragment } from "react";
-import meals from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import meals from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-const Header=()=>{
-    return<Fragment>
-        <header className={classes.header}>
-           
-            <h1>React meals</h1>
-            <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={meals} alt=""></img>
-        </div>
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt=""></img>
+      </div>
     </Fragment>
-}
+  );
+};
 
 export default Header;
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,8 +1,9 @@
-import classes from './MealsSummary.module.css';
+import classes from "./MealsSummary.module.css";
 
-const MealsSummary=()=>{
+const MealsSummary = () => {
+  return (
     <section className={classes.summary}>
-        <h2>Delicious Food, Delivered To You</h2>
+      <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
@@ -12,6 +13,7 @@ const MealsSummary=()=>{
         of course by experienced chefs!
       </p>
     </section>
-}
+  );
+};
 
 export default MealsSummary;
