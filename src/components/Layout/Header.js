fbc9217e3ed528bedb02f6a5e07e2aa1eefddcf0
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-01-31 11:23:29 +0530

    1)Created new Cart Button file for Button.2)Created CartIcon svg file and added it to button.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import meals from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 const Header=()=>{
     return<Fragment>
         <header className={classes.header}>
+           
             <h1>React meals</h1>
-            <button></button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={meals} alt=""></img>
