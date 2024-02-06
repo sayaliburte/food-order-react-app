7962600e3d30172526fb97c24cf42e0c91d0f21c
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-02-07 01:10:19 +0530

    1)Created modal component and added styling to it.2)Created portals for modal and backdrops.3)Added showcart and hidecart functionality.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+
+import { useState } from "react";
 import "./App.css";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 
 import Meals from "./components/Meals/Meals";
 
 
 function App() {
+
+  const [cartIsShown,setCartIsShown]=useState(false);
+
+  const showCartHandler=()=>{
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler=()=>{
+    setCartIsShown(false);
+  }
   return (
-    <div className="App">
-      <Header></Header>
+    
+    <div className='App'>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShownCart={showCartHandler}></Header>
 
       <main>
         <Meals />
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import { Fragment } from "react";
 import meals from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShownCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={meals} alt=""></img>
