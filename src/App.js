6d241130426e888302a0f8e1d68572bcea18ef4f
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-02-09 00:19:55 +0530

    1)Created cart-context and cart-provider for cart. 2)Used useContext wherever cart data is needed.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-
 import { useState } from "react";
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 
 import Meals from "./components/Meals/Meals";
-
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  const [cartIsShown,setCartIsShown]=useState(false);
-
-  const showCartHandler=()=>{
+  const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
-  const hideCartHandler=()=>{
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
   return (
-    
-    <div className='App'>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShownCart={showCartHandler}></Header>
 
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
 }
 
