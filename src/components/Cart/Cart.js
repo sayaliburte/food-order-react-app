60f568cb011a933fb16c2cee545dc71e52c6388d
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-02-09 23:22:32 +0530

    1)Added caritem component to style each cart element along with cartitem css file.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,48 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler=id=>{
+
+  }
+
+  const cartItemAddHandler=item=>{
+
+  }
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "m1", name: "Sushi", amount: 2, price: 140 }].map((meal) => (
-        <li>{meal.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        ></CartItem>
       ))}
     </ul>
   );
+
+ 
   return (
     <Modal onClose={props.onClose}>
-        {cartItems}
+      {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
